b3e8a8616cc76ae51d201ac6e7987510b62f25aa
LolaV <mdolores.villalobos@gmail.com>
2023-11-04 19:05:19 +0100

    Aplicación actualizada hasta sección 6 del curso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { CounterModule } from './counter/counter.module';
 import { HeroModule } from './heroes/hero.module';
+import { DbzModule } from './dbz/dbz.module';
 
 import { AppComponent } from './app.component';
 
@@ -14,7 +15,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     CounterModule,
-    HeroModule
+    HeroModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+import { v4 as uuid } from 'uuid';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  @Input()
+  public characterList: Character[] = [{
+    id: uuid(),
+    name:'Trunk',
+    power: 10
+  }]
+
+  // onDeleteCharacter(index:number):void {
+  //   this.onDelete.emit(index);
+  // }
+
+  onDeleteCharacter(id: string):void {
+    this.onDelete.emit(id);
+  }
+
+}
